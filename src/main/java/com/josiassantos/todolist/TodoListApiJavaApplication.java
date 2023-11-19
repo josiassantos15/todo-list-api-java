09dc55205f377e7c3902721cf12271b44723a8a0
Josiassantos1577 <josiassantos1577@gmail.com>
2023-11-19 14:20:15 -0300

    config: Inicializando o projeto com Spring Boot

---
+++ src/main/java/com/josiassantos/todolist/TodoListApiJavaApplication.java
@@ -0,0 +1,13 @@
+package com.josiassantos.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListApiJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josiassantos/todolist/TodoListApiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josiassantos.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListApiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
